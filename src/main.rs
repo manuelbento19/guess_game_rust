7b9e07c776f9394fa5abfe49dbed4042a8062b1b
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-05-13 14:13:26 +0100

    feat: Conditions - match

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use std::io::{stdin};
+use std::{cmp::Ordering, io::stdin};
 use rand::{thread_rng, Rng};
 
 fn main(){
     let secret_number = thread_rng().gen_range(0..7);
-    println!("Insert the number between 0 and 7: ");
-    let mut input = String::new();
-    stdin().read_line(&mut input,).expect("Expected receive a number");
-    println!("The secret number is {secret_number}");
-    println!("Your input is {}",input);
+    loop {
+        println!("Insert the number between 0 and 7: ");
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("Expected receive a number");
+
+        let input:u32 = match input.trim().parse(){
+            Ok(value) => value,
+            Err(_) => continue
+        };
+        match input.cmp(&secret_number){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            },
+        }
+    }
+
+   
 }
